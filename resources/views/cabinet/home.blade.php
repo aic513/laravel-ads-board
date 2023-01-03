d3be949ed2a9d9720d03979ed6b16565bd9ee564
aic513 <aic513@mail.ru>
2023-01-03 22:41:38 +0300

    add flashes

--- resources/views/cabinet/home.blade.php
+++ resources/views/cabinet/home.blade.php
@@ -11,12 +11,6 @@
         <div class="card-header">{{ __('Dashboard') }}</div>
 
         <div class="card-body">
-            @if (session('status'))
-                <div class="alert alert-success">
-                    {{ session('status') }}
-                </div>
-            @endif
-
             {{ __('You are logged in!') }}
         </div>
     </div>

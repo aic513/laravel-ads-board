b491b0176d8e84cbb94577f379e818fb30fcbf7a
aic513 <aic513@mail.ru>
2023-01-03 22:33:50 +0300

    add breadcrumbs

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,71 +1,77 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Reset Password') }}</div>
+    <ul class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('home')  }}">Home</a></li>
+        <li class="breadcrumb-item"><a href="{{ route('login')  }}">Login</a></li>
+        <li class="breadcrumb-item"><a href="{{ route('password.request')  }}">Reset</a></li>
+        <li class="breadcrumb-item active">Change</li>
+    </ul>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Reset Password') }}</div>
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('password.update') }}">
-                            @csrf
+                <div class="card-body">
+                    <form method="POST" action="{{ route('password.update') }}">
+                        @csrf
 
-                            <input type="hidden" name="token" value="{{ $token }}">
+                        <input type="hidden" name="token" value="{{ $token }}">
 
-                            <div class="row mb-3">
-                                <label for="email"
-                                       class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                        <div class="row mb-3">
+                            <label for="email"
+                                   class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email"
-                                           class="form-control @error('email') is-invalid @enderror" name="email"
-                                           value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+                            <div class="col-md-6">
+                                <input id="email" type="email"
+                                       class="form-control @error('email') is-invalid @enderror" name="email"
+                                       value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
 
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                    @enderror
-                                </div>
+                                @enderror
                             </div>
+                        </div>
 
-                            <div class="row mb-3">
-                                <label for="password"
-                                       class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                        <div class="row mb-3">
+                            <label for="password"
+                                   class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
-                                <div class="col-md-6">
-                                    <input id="password" type="password"
-                                           class="form-control @error('password') is-invalid @enderror" name="password"
-                                           required autocomplete="new-password">
+                            <div class="col-md-6">
+                                <input id="password" type="password"
+                                       class="form-control @error('password') is-invalid @enderror" name="password"
+                                       required autocomplete="new-password">
 
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                    @enderror
-                                </div>
+                                @enderror
                             </div>
+                        </div>
 
-                            <div class="row mb-3">
-                                <label for="password-confirm"
-                                       class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+                        <div class="row mb-3">
+                            <label for="password-confirm"
+                                   class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control"
-                                           name="password_confirmation" required autocomplete="new-password">
-                                </div>
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control"
+                                       name="password_confirmation" required autocomplete="new-password">
                             </div>
+                        </div>
 
-                            <div class="row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Reset Password') }}
-                                    </button>
-                                </div>
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Reset Password') }}
+                                </button>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
+    </div>
 @endsection
--- resources/views/cabinet/home.blade.php
+++ resources/views/cabinet/home.blade.php
@@ -1,17 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="card">
-            <div class="card-header">{{ __('Dashboard') }}</div>
+    <ul class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('home')  }}">Home</a></li>
+        <li class="breadcrumb-item active">Cabinet</li>
+    </ul>
+    <div class="card">
+        <div class="card-header">{{ __('Dashboard') }}</div>
 
-            <div class="card-body">
-                @if (session('status'))
-                    <div class="alert alert-success">
-                        {{ session('status') }}
-                    </div>
-                @endif
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
 
-                {{ __('You are logged in!') }}
-            </div>
+            {{ __('You are logged in!') }}
         </div>
+    </div>
 @endsection

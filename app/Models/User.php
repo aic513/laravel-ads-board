de7562a73e735d9f206a8224b2c847ffa7a31d65
aic513 <aic513@mail.ru>
2023-01-04 17:16:10 +0300

    add User property PHPDoc

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $status
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;

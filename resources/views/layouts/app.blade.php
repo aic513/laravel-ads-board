64dd98aad149b0afc9b8f122e3bcbe8ae3a2d130
aic513 <aic513@mail.ru>
2025-08-08 16:24:37 +0300

    extracted search partial

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,75 +1,60 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ __('Adverts') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ mix('js/app.js', 'build') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <title>Adverts</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css', 'build') }}" rel="stylesheet">
 </head>
 <body id="app">
 <header>
-    <nav class="navbar navbar-expand-md shadow-sm">
+    <nav class="navbar navbar-expand-md navbar-dark">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
-                {{ __('Adverts') }}
+                Adverts
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav me-auto">
+                <ul class="navbar-nav mr-auto">
 
                 </ul>
 
                 <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto">
+                <ul class="navbar-nav ml-auto">
                     <!-- Authentication Links -->
                     @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @endif
-
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
+                        <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+                        <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
                     @else
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('cabinet.home') }}">{{ __('Cabinet') }}</a>
-                                <a class="dropdown-item" href="{{ route('admin.home') }}">{{ __('Admin') }}</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('admin.home') }}">Admin</a>
+                                <a class="dropdown-item" href="{{ route('cabinet.home') }}">Cabinet</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
-                                   document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                                       document.getElementById('logout-form').submit();">
+                                    Logout
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
                                     @csrf
                                 </form>
                             </div>
@@ -81,34 +66,7 @@
     </nav>
 
     @section('search')
-        <div class="search-bar pt-3">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-9">
-                        <form action="{{ route('adverts.index') }}" method="GET">
-                            <div class="row">
-                                <div class="col-md-11">
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" name="text"
-                                               value="{{ request('text') }}" placeholder="Search for...">
-                                    </div>
-                                </div>
-                                <div class="col-md-1">
-                                    <div class="form-group">
-                                        <button class="btn btn-light border" type="submit"><span
-                                                class="fa fa-search"></span></button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="col-md-3" style="text-align: right">
-                        <p><a href="{{ route('cabinet.adverts.create') }}" class="btn btn-success"><span
-                                    class="fa fa-plus"></span> Add New Advertisement</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include('layouts.partials.search', ['category' => null, 'route' => route('adverts.index')])
     @show
 </header>
 
@@ -124,9 +82,12 @@
 <footer>
     <div class="container">
         <div class="border-top pt-3">
-            <p>&copy; {{ date('Y') }} - {{ __('Adverts') }}</p>
+            <p>&copy; {{ date('Y') }} - Adverts</p>
         </div>
     </div>
 </footer>
+
+<!-- Scripts -->
+<script src="{{ mix('js/app.js', 'build') }}"></script>
 </body>
 </html>

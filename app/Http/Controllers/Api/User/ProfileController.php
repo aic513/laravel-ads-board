523d83726e61ce281808b63c149bcccf7acf9c6e
aic513 <aic513@mail.ru>
2026-04-22 19:46:32 +0300

    migrated to json resources

--- app/Http/Controllers/Api/User/ProfileController.php
+++ app/Http/Controllers/Api/User/ProfileController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Cabinet\ProfileEditRequest;
-use App\Http\Serializers\UserSerializer;
+use App\Http\Resources\ProfileResource;
 use App\Models\User;
 use App\UseCases\Profile\ProfileService;
 use Illuminate\Http\Request;
@@ -13,28 +13,22 @@ use Illuminate\Http\Request;
 class ProfileController extends Controller
 {
     private $service;
-    /**
-     * @var UserSerializer
-     */
-    private $serializer;
 
-    public function __construct(ProfileService $service, UserSerializer $serializer)
+    public function __construct(ProfileService $service)
     {
         $this->service = $service;
-        $this->serializer = $serializer;
     }
 
-    public function show(Request $request): array
+    public function show(Request $request)
     {
-        $user = $request->user();
-        return $this->serializer->profile($user);
+        return new ProfileResource($request->user());
     }
 
-    public function update(ProfileEditRequest $request): array
+    public function update(ProfileEditRequest $request)
     {
         $this->service->edit($request->user()->id, $request);
 
         $user = User::findOrFail($request->user()->id);
-        return $this->serializer->profile($user);
+        return new ProfileResource($user);
     }
 }

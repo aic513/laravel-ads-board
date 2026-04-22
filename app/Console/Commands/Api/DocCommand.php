2402748f7997258b58615dee573e521ea928928e
aic513 <aic513@mail.ru>
2026-04-22 22:21:47 +0300

    generated swagger jSON

---
+++ app/Console/Commands/Api/DocCommand.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Console\Command;
+
+class DocCommand extends Command
+{
+    protected $signature = 'api:doc';
+
+    public function handle(): void
+    {
+        $swagger = base_path('vendor/bin/swagger');
+        $source = app_path('Http');
+        $target = public_path('docs/swagger.json');
+
+        passthru('"' . PHP_BINARY . '"' . " \"{$swagger}\" \"{$source}\" --output \"{$target}\"");
+    }
+}

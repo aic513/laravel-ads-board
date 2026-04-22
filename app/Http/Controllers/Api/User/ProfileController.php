7efa465791529228610718b9cbd6977172402a2b
aic513 <aic513@mail.ru>
2026-04-22 15:35:58 +0300

    added API profile controller

---
+++ app/Http/Controllers/Api/User/ProfileController.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\ProfileEditRequest;
+use App\Models\User;
+use App\UseCases\Profile\ProfileService;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    private $service;
+
+    public function __construct(ProfileService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function show(Request $request)
+    {
+        return $request->user();
+    }
+
+    public function update(ProfileEditRequest $request)
+    {
+        $this->service->edit($request->user()->id, $request);
+
+        return User::findOrFail($request->user()->id);
+    }
+}

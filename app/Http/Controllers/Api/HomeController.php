82a7851ae5d990441e7fbf19b06cf0b559b921ab
aic513 <aic513@mail.ru>
2026-04-22 15:24:58 +0300

    added api home controller

---
+++ app/Http/Controllers/Api/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+class HomeController
+{
+    public function home()
+    {
+        return [
+            'name' => 'Board API',
+        ];
+    }
+}

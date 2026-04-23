71e8e8a52abe2965948e47b0e39c16018c541ca4
aic513 <aic513@mail.ru>
2026-04-23 22:24:24 +0300

    separated listeners

---
+++ app/Listeners/Advert/AdvertChangedListener.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Listeners\Advert;
+
+use App\Services\Search\AdvertIndexer;
+
+class AdvertChangedListener
+{
+    private $indexer;
+
+    public function __construct(AdvertIndexer $indexer)
+    {
+        $this->indexer = $indexer;
+    }
+
+    public function handle($event): void
+    {
+        $this->indexer->index($event->advert);
+    }
+}

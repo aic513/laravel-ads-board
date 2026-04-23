911329f74658bbcc55b44c912d674b7a707053b9
aic513 <aic513@mail.ru>
2026-04-23 22:26:16 +0300

    added advert reindex job

---
+++ app/Jobs/Advert/ReindexAdvert.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Jobs\Advert;
+
+use App\Models\Adverts\Advert\Advert;
+use App\Services\Search\AdvertIndexer;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ReindexAdvert implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $advert;
+
+    public function __construct(Advert $advert)
+    {
+        $this->advert = $advert;
+    }
+
+    public function handle(AdvertIndexer $indexer): void
+    {
+        $indexer->index($this->advert);
+    }
+}
